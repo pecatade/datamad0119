002843b55df31bd49e7442d0553ab0afd68f195c
Pablo Ramos <pabloramosgomez@outlook.es>
2019-01-23 11:14:40 +0100

    Fichero eliminado

--- module-1/lab-mysql-select/your-code/solutions.sql
+++
@@ -1,8 +0,0 @@
-SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
-FROM authors
-INNER JOIN titleauthor
-ON authors.au_id = titleauthor.au_id
-INNER JOIN titles
-ON titles.title_id = titleauthor.title_id
-INNER JOIN publishers
-ON titles.pub_id = publishers.pub_id
