1437271ce3d16cce99682057b65dbb9ec2212293
Pablo Ramos <pabloramosgomez@outlook.es>
2019-01-23 16:11:21 +0100

    1º Challenge Terminado

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,9 +1,32 @@
-SELECT authors.au_id 'AUTHOR ID', authors.au_lname 'LAST NAME', authors.au_fname 'FIRST NAME', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS PROFIT
-FROM authors
-LEFT JOIN titleauthor
-ON authors.au_id = titleauthor.au_id
+#Challenge 1
+#Step 1
+
+SELECT titles.title_id 'TITLE ID', titleauthor.au_id 'AUTHOR ID', titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 'Royalty'
+FROM titles
+INNER JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id;
+
+
+#Step 2
+SELECT titles.title_id 'TITLE ID', titleauthor.au_id 'AUTHOR ID', SUM(titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100) 'PROFITS'
+FROM titleauthor
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id 
+GROUP BY titles.title_id, titleauthor.au_id;
+
+
+#Step 3
+
+SELECT titleauthor.au_id 'AUTHOR ID', SUM(titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100) 'PROFITS'
+FROM titleauthor
 LEFT JOIN sales
 ON titleauthor.title_id = sales.title_id
 LEFT JOIN titles
-ON titles.title_id = titleauthor.title_id
-GROUP BY AUTHORS.AU_ID
+ON titleauthor.title_id = titles.title_id 
+GROUP BY titleauthor.au_id
+ORDER BY PROFITS DESC
+LIMIT 3
