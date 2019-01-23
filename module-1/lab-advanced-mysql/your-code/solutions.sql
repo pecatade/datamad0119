e197a03bb861b6b6188ccb7be291723b5bee7db1
Pablo Ramos <pabloramosgomez@outlook.es>
2019-01-23 16:33:25 +0100

    Challeng 2 con algunas correcciones del challenge 1

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,13 +1,11 @@
 #Challenge 1
 #Step 1
-
-SELECT titles.title_id 'TITLE ID', titleauthor.au_id 'AUTHOR ID', titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 'Royalty'
-FROM titles
-INNER JOIN titleauthor
-ON titles.title_id = titleauthor.title_id
+SELECT titles.title_id 'TITLE ID', titleauthor.au_id 'AUTHOR ID', titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 'ROYALTY'
+FROM titleauthor
 INNER JOIN sales
-ON titleauthor.title_id = sales.title_id;
-
+ON titleauthor.title_id = sales.title_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id;
 
 #Step 2
 SELECT titles.title_id 'TITLE ID', titleauthor.au_id 'AUTHOR ID', SUM(titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100) 'PROFITS'
@@ -18,15 +16,36 @@ INNER JOIN titles
 ON titleauthor.title_id = titles.title_id 
 GROUP BY titles.title_id, titleauthor.au_id;
 
-
 #Step 3
-
-SELECT titleauthor.au_id 'AUTHOR ID', SUM(titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100) 'PROFITS'
+SELECT titleauthor.au_id 'AUTHOR ID', SUM(titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100) 'PROFITS PER AUTHOR'
 FROM titleauthor
 LEFT JOIN sales
 ON titleauthor.title_id = sales.title_id
 LEFT JOIN titles
 ON titleauthor.title_id = titles.title_id 
 GROUP BY titleauthor.au_id
-ORDER BY PROFITS DESC
-LIMIT 3
+ORDER BY 'PROFITS PER AUTHOR' DESC
+LIMIT 3;
+
+#Challenge 2
+#Step 1
+
+CREATE TEMPORARY TABLE royalty_tables
+SELECT titles.title_id 'TITLE ID', titleauthor.au_id 'AUTHOR ID', titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 'ROYALTY'
+FROM titles
+INNER JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id;
+
+#Step 2
+SELECT `TITLE ID`, `AUTHOR ID`, SUM(`ROYALTY`) 'PROFITS'
+FROM royalty_tables
+GROUP BY `TITLE ID`, `AUTHOR ID`;
+
+#Step 3
+SELECT `AUTHOR ID`, SUM(`Royalty`) 'PROFITS PER AUTHOR'
+FROM royalty_tables
+GROUP BY `AUTHOR ID`
+ORDER BY 'PROFITS PER AUTHOR' DESC
+LIMIT 3;
