768fe5f85a9ec5218aa8b060e4f882861d07e841
Pablo Ramos <pabloramosgomez@outlook.es>
2019-01-23 12:04:25 +0100

    Algunos cambios de renombre

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,20 +1,21 @@
 # Challenge 1
-# SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
-# FROM authors
-# INNER JOIN titleauthor
-# ON authors.au_id = titleauthor.au_id
-# INNER JOIN titles
-# ON titles.title_id = titleauthor.title_id
-# INNER JOIN publishers
-# ON titles.pub_id = publishers.pub_id;
+#SELECT authors.au_id 'AUTHOR ID', authors.au_lname 'LAST NAME', authors.au_fname 'FIRST NAME', titles.title 'TITLE', publishers.pub_name 'PUBLISHER'
+#FROM authors
+#INNER JOIN titleauthor
+#ON authors.au_id = titleauthor.au_id
+#INNER JOIN titles
+#ON titles.title_id = titleauthor.title_id
+#INNER JOIN publishers
+#ON titles.pub_id = publishers.pub_id;
 
 # Challenge 2
-# SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name AS `AUTHOR ID`, COUNT(titles.title) AS `TITLE COUNT`
-# FROM authors
-# INNER JOIN titleauthor
-# ON authors.au_id = titleauthor.au_id
-# INNER JOIN titles
-# ON titles.title_id = titleauthor.title_id
-# INNER JOIN publishers
-# ON titles.pub_id = publishers.pub_id
-# GROUP BY au_id
+SELECT authors.au_id 'AUTHOR ID', authors.au_lname 'LAST NAME', authors.au_fname 'FIRST NAME', titles.title 'TITLE', publishers.pub_name 'PUBLISHER', 
+COUNT(titles.title) AS 'TITLE COUNT'
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers
+ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id
